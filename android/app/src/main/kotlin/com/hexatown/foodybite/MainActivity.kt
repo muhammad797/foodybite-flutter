6c8161160589e0847f380f0acccdec8ad7c546c3
thehexatown <thehexatown@gmail.com>
2020-10-21 16:25:17 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/hexatown/foodybite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hexatown.foodybite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "FoodyBite Clone",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.greenAccent,
+          body: Container(
+            child: Text("Hello World"),
+          ),
+        ));
+  }
+}
